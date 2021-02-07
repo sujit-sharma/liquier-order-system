0fa4737327a9b4058bbd1e8852ea9c2b3a5939d9
sujit <sujit.sharma@clusus.com>
2021-02-07 12:28:03 +0545

    Initial commit

---
+++ src/main/java/com/sujit/ordersystemspringweb/OrderSystemSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.sujit.ordersystemspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSystemSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSystemSpringWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sujit/ordersystemspringweb/OrderSystemSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujit.ordersystemspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
